b5d1f1da19c73f8820ff415bf4c5135cc08628b2
Shivam Panigrahi <73484687+shivam821@users.noreply.github.com>
2025-03-17 18:02:23 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-south-1_HlHiAFKOc', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'u4g6lur5obed718q5ldnpg5tb', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-2
     },
     api: {
